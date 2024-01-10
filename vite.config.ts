b4e228bd67d1cff4c6946a6cf5378177d09b80b5
Jamie <jamie.haywood@gmail.com>
2024-01-10 22:29:46 +0000

    chore: add react to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import devServer from "@hono/vite-dev-server";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
+    react(),
+    vike(),
     devServer({
       entry: "src/index.ts",
     }),
-    vike(),
   ],
 });
