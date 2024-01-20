2273c638924c107e92a5b058ef3f4120aa946e24
Jamie <jamie.haywood@gmail.com>
2024-01-20 12:11:48 +0000

    feat: add hello api route

---
+++ src/api/hello.ts
@@ -0,0 +1,8 @@
+import { Context, Next } from "hono"
+
+
+export const hello = async (context: Context, next: Next) => {
+	const name = context.req.query('name')
+
+	return new Response(`Hello ${name ?? 'World'}`)
+}
